f6e22dd54e2b27f8501421a500d169f58558363d
ankur1324 <pandeyankur55143@gmail.com>
2023-08-04 08:38:17 +0530

    deploy changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
